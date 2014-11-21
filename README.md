5ab17b994026d545dbb5328e2660b5ad75888f35
Timothy Macdonald <tmacdonald@groupon.com>
2014-11-20 22:13:14 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# abclj
+
+A simple ABC parser for Clojure.
+
+## License
+
+Copyright © 2014 Timothy S. Macdonald
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject abclj "0.1.0-SNAPSHOT"
+  :description "A simple ABC parser for Clojure"
+  :url "http://github.com/tsmacdonald/abclj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [instaparse "1.3.2"]])
